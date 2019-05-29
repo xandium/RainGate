362a2d3850e80f705e33699634bcdf323e6b75c4
Sharon Williams <swilliams@xandium.io>
2019-05-29 18:25:14 -0400

    Formatting fix and package naming change

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
-const config = require('./config/config')
-const RainCache = require('raincache')
-const AmqpConnector = require('./DetailedAmqpConnector')
-const Redis = require('raincache').Engines.RedisStorageEngine
-let StatsD
-let statsClient
+const config = require("./config/config");
+const RainCache = require("@xandium/raincache");
+const AmqpConnector = require("./DetailedAmqpConnector");
+const Redis = require("raincache").Engines.RedisStorageEngine;
+let StatsD;
+let statsClient;
 try {
-  StatsD = require('hot-shots')
-} catch (e) {
-
-}
+  StatsD = require("hot-shots");
+} catch (e) {}
 if (StatsD && config.statsD && config.statsD.enabled) {
-  statsClient = new StatsD(config.statsD)
+  statsClient = new StatsD(config.statsD);
 }
-const con = new AmqpConnector(config.amqp, statsClient)
-const cache = new RainCache({ storage: { default: new Redis(config.redis) }, debug: false }, con, con)
+const con = new AmqpConnector(config.amqp, statsClient);
+const cache = new RainCache(
+  { storage: { default: new Redis(config.redis) }, debug: false },
+  con,
+  con
+);
 const init = async () => {
-  await cache.initialize()
-}
-init().then(async () => {
-  console.log('Initialized successfully')
-}).catch(e => console.error(e))
+  await cache.initialize();
+};
+init()
+  .then(async () => {
+    console.log("Initialized successfully");
+  })
+  .catch(e => console.error(e));
